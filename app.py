5106672754115e8d4e502d5c953fa75120fb7ed9
Manoj Kapkoti <manojkapkoti1008@gmail.com>
2026-05-29 08:29:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,3 @@
-Face Authentication API
-
-A FastAPI-based face verification service using InsightFace.
-
-Features
-Upload two face images
-Detect faces
-Extract embeddings
-Compute similarity score
-Verify whether both images belong to the same person
-Return face bounding boxes
-
-Tech Stack
-Python
-FastAPI
-InsightFace
-OpenCV
 
 installation 
 git clone https://github.com/your-username/face-authentication.git cd face-authentication
