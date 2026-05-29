f5237482e68dd41527c743d9a08ba7ac51395237
Manoj Kapkoti <manojkapkoti1008@gmail.com>
2026-05-29 08:26:19 +0530

    Main.py

---
+++ Main.py
@@ -0,0 +1,17 @@
+Recommended Repository Structure
+
+face-authentication/
+│
+├── app/
+│   ├── main.py
+│   ├── face_verify.py
+│   ├── utils.py
+│   └── models/
+│
+├── sample_images/
+│   ├── person1.jpg
+│   └── person2.jpg
+│
+├── requirements.txt
+├── README.md
+└── .gitignore
